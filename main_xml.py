a33ee7118fbd6770acf8755df0eaefee6c3f16e4
david howard <dbrannonhoward@gmail.com>
2020-02-29 15:10:45 -0500

    fetches root from xml file located at PycharmProjects/xml_files

--- main_xml.py
+++ main_xml.py
@@ -1,6 +1,6 @@
-from directories import filter_file_list_by_ext, initialize_directories, \
-    list_of_files_in_dir, print_directories
+from directories import *
 from file_extensions import *
+from xml_methods import *
 
 filter_extension = xml_extension
 
@@ -10,12 +10,24 @@ print_directories(cdir, pdir, odir, xdir)
 
 # Get list() of files from directory, print it
 xdir_list = list_of_files_in_dir(xdir)
-print 'The file list is : ' + str(xdir_list)
+print 'The unfiltered file list in ' + str(xdir) + ' is : ' + str(xdir_list)
 # Filter files of a specific extension into a list(), print it
 filtered_file_list = filter_file_list_by_ext(xdir_list, filter_extension)
-print filtered_file_list
+print 'The filtered file list from ' + str(xdir) + ' is : ' + str(filtered_file_list)
 
-# Check to see if a file is XML
+# Change working dir to XML dir
+change_cwd_to_xml_dir(xdir)
+print 'The current working directory is : ' + str(os.getcwd())
+
+# Check to see if any file in the dir is an XML file
+# Print the contents to terminal
+for filtered_file_name in filtered_file_list:
+    print 'The filtered file name is ' + str(filtered_file_name)
+    xml_root = get_xml_tree_root(filtered_file_name)
+    print(type(xml_root))
+
+
+
 
 
 # / eof / #
--- xml_methods.py
+++ xml_methods.py
@@ -1,1 +1,34 @@
-import xml.etree.ElementTree
+import xml.etree.ElementTree as ET
+from file_extensions import xml_extension
+import os
+
+
+def change_cwd_to_xml_dir(xml_files_dir):
+    os.chdir(xml_files_dir)
+
+
+def get_xml_tree_root(xml_file_name):
+    xml_root = ET.parse(xml_file_name).getroot()
+    return xml_root
+
+
+def is_xml_file(file_name):
+    try:
+        if str(file_name).endswith(xml_extension):
+            # print file_name + ' IS an XML file'
+            return True
+        else:
+            # print file_name + ' IS NOT an XML file'
+            return False
+    except Exception as e:
+        print str(e) + ' : could not determine if file is XML file'
+
+
+def output_xml_contents_to_console(xml_file_name):
+    print str(os.getcwd())
+    with open(xml_file_name) as xml_file:
+        print xml_file.read()
+
+
+
+# / eof / #
